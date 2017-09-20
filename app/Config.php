5130260bd99c6a795e6560aa58c6416ac6e6c598
Parvez M Robin <parvezmrobin@gmail.com>
2017-09-20 12:04:58 +0600

    Config class redefined

--- app/Config.php
+++ app/Config.php
@@ -12,8 +12,37 @@ use Symfony\Component\Yaml\Yaml;
 
 class Config
 {
-    public static function get($config, $key)
+    private $filePath;
+    private $content;
+    private $data;
+
+
+    /**
+     * Config constructor.
+     * @param $config
+     */
+    function __construct($config)
     {
-        return Yaml::parse(file_get_contents(__DIR__ . "/../config/" . $config . ".ymal"))[$key];
+        $this->filePath = __DIR__ . "/../config/" . $config . ".ymal";
+        $this->content = file_get_contents($this->filePath);
+        $this->data = Yaml::parse($this->content);
+    }
+
+    /**
+     * @param string $key
+     * @return string
+     */
+    public function get($key)
+    {
+        return $this->data[$key];
+    }
+
+    /**
+     * @param string $config
+     * @return static
+     */
+    public static function from($config)
+    {
+        return new static($config);
     }
 }
