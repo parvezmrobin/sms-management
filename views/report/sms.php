9b8755ab8b6096d3faedeaba610d49de6aaac1dc
Parvez M Robin <parvezmrobin@gmail.com>
2017-10-22 23:14:23 +0600

    sms report front-end

---
+++ views/report/sms.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Parvez
+ * Date: 10/3/2017
+ * Time: 11:38 PM
+ */
+
+include_once __DIR__ . "/../../layout/header.php";
+
+?>
+
+<h1>SMS Report</h1>
+<hr>
+
+<?php
+include_once __DIR__ . "/../../layout/footer.php"; ?>
