6f4a696c04a43d7149c3adccaf1dfd90f67c2fe5
Parvez M Robin <parvezmrobin@gmail.com>
2017-11-09 19:22:23 +0600

    Excel detail report

--- views/report/campaign.php
+++ views/report/campaign.php
@@ -12,7 +12,7 @@ $query = new \Symfony\Component\HttpFoundation\ParameterBag($_GET);
     <link rel="stylesheet" href="./../../css/datatables.min.css">
     <h1>Campaign Report</h1>
     <br>
-    <form class="form-horizontal" action="campaign.php" method="get">
+    <form class="form-horizontal panel panel-default panel-body" action="campaign.php" method="get">
         <div class="form-group col-md-6">
             <label for="date-from" class="col-md-2 control-label">From</label>
             <div class="col-md-10">
@@ -68,8 +68,7 @@ if ($query->has('search')) {
 
 $campaigns = \DbModel\Model::where('campaign', implode(" AND ", $conditions));
 ?>
-    <br>
-    <div class="row">
+    <div class="panel panel-primary panel-body">
         <table class="table" id="campaigns">
             <thead>
             <tr>
---
+++ views/report/excel.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * User: Parvez
+ * Date: 11/9/2017
+ * Time: 5:04 PM
+ */
+include_once __DIR__ . "/../../layout/header.php";
+
+$query = new \Symfony\Component\HttpFoundation\ParameterBag($_GET);
+
+if (!$id = $query->get('id')) {
+    (new \Symfony\Component\HttpFoundation\RedirectResponse('campaign.php'))->send();
+}
+
+$excelSms = \DbModel\Model::find("excel_sms", $id);
+
+$dataFields = $excelSms->oneToMany('excel_sms_data', 'id');
+
+$data = "";
+foreach ($dataFields as $item) {
+    $data .= $item->data;
+}
+
+$csv = \League\Csv\Reader::createFromString($data);
+$sheet = (new \League\Csv\Statement())
+    ->offset(1)
+    ->process($csv);
+
+$header = (new \League\Csv\Statement())
+    ->limit(1)
+    ->process($csv)
+    ->jsonSerialize()[0];
+
+$numberColumnKey = array_search($excelSms->number, $header);
+
+$SMSs = []; //Contains the generated SMSs
+
+$columnCount = count($header);
+
+foreach ($sheet as $row) {
+    $sms = $excelSms->body;
+
+    foreach ($row as $index => $value) {
+        $sms = str_replace("{{{$header[$index]}}}", $value, $sms);
+    }
+
+    $SMSs[$row[$numberColumnKey]] = $sms;
+}
+
+//dump($SMSs);
+?>
+
+<h2>
+    Excel SMS Report
+    <small> : <?= \DbModel\Model::find('campaign', $id)->campaign_name ?></small>
+</h2>
+<hr>
+
+<table class="table" id="">
+    <thead>
+    <tr>
+        <th>Number</th>
+        <th>SMS</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($SMSs as $number => $SMS): ?>
+        <tr>
+            <td><?= $number ?></td>
+            <td><?= $SMS ?></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+
+
+<?php
+include_once __DIR__ . "/../../layout/footer.php"; ?>
